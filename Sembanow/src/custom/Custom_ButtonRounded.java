ca5f050789f637b0099de7d465de2e0b2c7f142b
Muhammad Fitrah Abdul Rahman Saleh <mfitrah15ofcc@gmail.com>
2025-04-21 10:11:28 +0700

    Merge origin/main into main
    # Conflicts:
    #	Sembanow/nbproject/project.properties
    #	Sembanow/src/com/raven/form/Form_Karyawan.form
    #	Sembanow/src/com/raven/form/Form_Karyawan.java
    #	Sembanow/src/com/raven/form/Form_Pelanggan.form
    #	Sembanow/src/com/raven/form/Form_Pelanggan.java
    #	Sembanow/src/com/raven/form/Form_Supplier.form
    #	Sembanow/src/com/raven/form/Form_Supplier.java
    #	Sembanow/src/com/raven/form/Form_editKaryawan.form
    #	Sembanow/src/com/raven/form/Form_editKaryawan.java
    #	Sembanow/src/com/raven/form/Form_editPelanggan.form
    #	Sembanow/src/com/raven/form/Form_editPelanggan.java
    #	Sembanow/src/com/raven/form/Form_editSupplier.form
    #	Sembanow/src/com/raven/form/Form_editSupplier.java
    #	Sembanow/src/com/raven/main/Main.java

---
+++ Sembanow/src/custom/Custom_ButtonRounded.java
@@ -0,0 +1,154 @@
+package custom;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashSet;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+public class Custom_ButtonRounded extends JButton{
+
+    private boolean over;
+    private Color fill;
+    private Icon icon;
+    
+    private Color fillOriginal;
+    private Color fillOver;
+    private Color fillClick;
+    private int strokeWidth;
+    private int roundedCorner;
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+        repaint();
+    }
+
+    public Color getFillOriginal() {
+        return fillOriginal;
+    }
+
+    public void setFillOriginal(Color fillOriginal) {
+        this.fillOriginal = fillOriginal;
+        if(!over){
+            fill = fillOriginal;
+            repaint();
+        }
+    }
+
+    public Color getFillOver() {
+        return fillOver;
+    }
+
+    public void setFillOver(Color fillOver) {
+        this.fillOver = fillOver;
+        if(over){
+            fill = fillOver;
+            repaint();
+        }
+    }
+
+    public Color getFillClick() {
+        return fillClick;
+    }
+
+    public void setFillClick(Color fillClick) {
+        this.fillClick = fillClick;
+        if(over){
+            fill = fillClick;
+            repaint();
+        }
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+        repaint();
+    }
+
+    public int getRoundedCorner() {
+        return roundedCorner;
+    }
+
+    public void setRoundedCorner(int roundedCorner) {
+        this.roundedCorner = roundedCorner;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+       if(!isOpaque()){
+           Graphics2D g2d = (Graphics2D) g;
+           g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+           int s = strokeWidth;
+           int w = getWidth() - (2*s);
+           int h = getHeight() - (2*s);
+           
+           g2d.setColor(fill);
+           g2d.fillRoundRect(s, s, w, h, roundedCorner, roundedCorner);
+       }
+       super.paintComponent(g);
+    }
+    
+    
+    public Custom_ButtonRounded() {
+        fillOriginal = new Color(18,45,79);
+        fillOver = new Color(44,127,183);
+        fillClick = new Color(30,92,133);
+        strokeWidth = 2;
+        roundedCorner = 15;
+        fill = fillOriginal;
+        icon = null;
+        
+        setOpaque(false);
+        setBorder(null);
+        setFocusPainted(false);
+        setContentAreaFilled(false);
+        setBackground(fillOriginal);
+        setForeground(Color.WHITE);
+        
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                fill = fillOver;
+                over = true;
+                repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                fill = fillClick;
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            if(over){
+                fill = fillOver;
+            }else {
+                fill = fillOriginal;
+            }
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                fill = fillOriginal;
+                over = false;
+                repaint();
+            }
+            
+        });
+    }
+
+}
